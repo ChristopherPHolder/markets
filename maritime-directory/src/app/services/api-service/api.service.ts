3ff22553aee063d9d15c4388eb1427f1e2e56074
Chris <chris@deep-blue.io>
2022-09-29 01:28:33 +0200

    wip: add preload tag

--- maritime-directory/src/app/services/api-service/api.service.ts
+++ maritime-directory/src/app/services/api-service/api.service.ts
@@ -1,9 +1,11 @@
 
 import { PLATFORM_ID, Injectable, Inject } from "@angular/core";
 import { isPlatformBrowser } from '@angular/common';
-import { Observable, of, shareReplay } from "rxjs";
+import { map, Observable, of, shareReplay, tap } from "rxjs";
+import { fromFetch } from "rxjs/fetch";
 import { ListingOverview } from "src/app/mock-highlights";
-import { HttpClient } from "@angular/common/http";
+import { LinkService } from "../link-service/link.service";
+/* import { HttpClient } from "@angular/common/http"; */
 
 @Injectable()
 export class ApiService {
@@ -16,13 +18,30 @@ export class ApiService {
 
   constructor(
     @Inject(PLATFORM_ID) platformId: object,
-    private http: HttpClient
+    private linkService: LinkService,
   ) {
     this.isBrowser = isPlatformBrowser(platformId);
   }
 
+  addPreloadLCPLinks():void {
+    this.getHighlightListings().pipe(tap((listings) => {
+      if (!listings) {
+        return;
+      }
+      for (let i = 0; i < 2; i++) {
+        this.linkService.addLink({
+          rel: 'preload',
+          as: 'image',
+          href: listings[i].thumbnailUrl,
+          fetchpriority: "high"
+        })
+      }
+    })).subscribe();
+  }
+
   getHighlightListings(): Observable<ListingOverview[] | null> {
     if (!this.isBrowser) {
+      this.addPreloadFetchLink();
       return of(null);
     }
     if (!this.highlights$) {
@@ -31,7 +50,25 @@ export class ApiService {
     return  this.highlights$;
   }
 
-  private fetchHighlights() {
-    return this.http.get<ListingOverview[]>(this.endpoint).pipe(shareReplay());
+  private fetchHighlights(): Observable<ListingOverview[]> {
+    /* return this.http.get<ListingOverview[]>(this.endpoint).pipe(shareReplay()); */
+    return fromFetch<ListingOverview[]>(this.endpoint, { 
+      selector: response => response.json() 
+    }).pipe( 
+      shareReplay(),
+      map((listings) => {
+        return listings;
+      })
+    );
+  }
+
+  private addPreloadFetchLink(): void {
+    this.linkService.addLink({
+      rel: 'preload',
+      as: 'fetch',
+      type: 'application/json',
+      href: this.endpoint,
+      crossorigin: ''
+    });
   }
 }
