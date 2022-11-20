3e1930c55311390287ea97fe34d2b4998a897c44
Chris <chris@deep-blue.io>
2022-11-20 16:50:11 +0100

    wip: add cy test for footer c

--- markets/apps/maritime-e2e/src/e2e/app.cy.ts
+++ markets/apps/maritime-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,10 @@
-import { getGreeting } from '../support/app.po';
+import { getFooter } from '../support/app.po';
 
 describe('maritime', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
+  it('should display footer', () => {
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome maritime');
+    getFooter().contains('Welcome maritime');
   });
 });
