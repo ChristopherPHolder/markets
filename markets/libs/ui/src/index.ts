a2f92265f657e4acab8e846cb975c3b7b1157c2d
Chris <chris@deep-blue.io>
2022-11-22 22:26:37 +0100

    feat: add header

--- markets/libs/ui/src/index.ts
+++ markets/libs/ui/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/ui.module';
 
 export * from './lib/footer/footer.component';
+
+export * from './lib/header/header.component';
---
+++ markets/libs/ui/src/lib/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'markets-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HeaderComponent {}
