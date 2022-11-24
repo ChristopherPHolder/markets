3bda2911fc8082250770c61ad76a06e41da1389f
Chris <chris@deep-blue.io>
2022-11-24 16:22:23 +0100

    feat: add listings routing module

--- markets/libs/listings/src/lib/listings.module.ts
+++ markets/libs/listings/src/lib/listings.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListingsRoutingModule } from './listings-routing.module'
 import { HighlightsComponent } from './highlights/highlights.component';
 import { HlListingsComponent } from './hl-listings/hl-listings.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ListingsRoutingModule],
   declarations: [HighlightsComponent, HlListingsComponent],
   exports: [HighlightsComponent, HlListingsComponent],
 })
