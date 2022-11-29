b2d3deee2e40a6513952b488cc786350ce069217
Chris <chris@deep-blue.io>
2022-11-29 18:41:50 +0100

    refactor: improve typing and remove on init

--- libs/listings/src/lib/highlights/highlights.component.ts
+++ libs/listings/src/lib/highlights/highlights.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { ListingPreview } from '@markets/api-service';
@@ -9,12 +9,11 @@ import { ListingPreview } from '@markets/api-service';
   styleUrls: ['./highlights.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class HighlightsComponent implements OnInit {
-  highlights$?: Observable<ListingPreview[] | null>;
+export class HighlightsComponent {
+  public readonly highlights$?: Observable<ListingPreview[] | null>;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    this.highlights$ = this.activatedRoute.data.pipe(map(data => data['highlights$']));
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.highlights$ = this.activatedRoute.data
+      .pipe(map(data => data['highlights$']));
   }
 }
