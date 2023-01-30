a08b3914e1e8819e979ef13ad5d5e8a76a057bc2
Chris <chris@deep-blue.io>
2023-01-30 23:10:39 +0100

    refactor: convert api service into data access into buildable lib with api service

--- libs/api-service/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/api-service';
-
-export * from './lib/types';
--- libs/api-service/src/lib/api-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apiService } from './api-service';
-
-describe('apiService', () => {
-  it('should work', () => {
-    expect(apiService()).toEqual('api-service');
-  });
-});
