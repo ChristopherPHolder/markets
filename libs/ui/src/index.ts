05526534146300cd92d0118abe8d58818cf64637
Chris <chris@deep-blue.io>
2023-01-31 01:13:59 +0100

    refactor: reduce spec import paths

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,8 +1,7 @@
 export * from './lib/header/header.component';
-
+export * from './lib/footer/footer.component';
 // Components
 export * from './lib/components/preview-listing/preview-listing.component';
 export * from './lib/components/preview-listing-ghost/preview-listing-ghost.component';
-
 // Patterns
 export * from './lib/patterns/highlights-listings-grid/highlights-listings-grid.component';
--- libs/ui/src/lib/footer/footer.component.spec.ts
+++ libs/ui/src/lib/footer/footer.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import { FooterComponent } from 'ui';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
