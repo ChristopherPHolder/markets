27e4f4522ddfa734357ff5e7b2d923c43cd34e55
Chris <chris@deep-blue.io>
2023-02-02 21:45:22 +0100

    refactor: remove link service from api service

--- libs/data-access/src/lib/api.service.ts
+++ libs/data-access/src/lib/api.service.ts
@@ -1,10 +1,7 @@
-import { PLATFORM_ID, Injectable, Inject } from "@angular/core";
-import { isPlatformBrowser } from '@angular/common';
+import { Injectable } from "@angular/core";
 
-import { map, Observable, of, shareReplay } from 'rxjs';
+import { map, Observable, shareReplay } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
-
-import { LinkService } from 'external-resource-link';
 import { environment } from 'environments';
 
 import { HighlightListingsPreviews } from "./types";
@@ -13,25 +10,13 @@ import { HighlightListingsPreviews } from "./types";
 export class ApiService {
   private readonly apiEndpoint = environment.apiEndpoint;
   private highlights$?:  Observable<HighlightListingsPreviews>;
-  readonly isBrowser: boolean;
-
-  constructor(
-    @Inject(PLATFORM_ID) platformId: object,
-    private linkService: LinkService,
-  ) {
-    this.isBrowser = isPlatformBrowser(platformId);
-  }
-  getHighlightListings(): Observable<HighlightListingsPreviews | null> {
-    if (!this.isBrowser) {
-      this.addPreloadFetchLink();
-      return of(null);
-    }
+  constructor() {}
+  getHighlightListings(): Observable<HighlightListingsPreviews> {
     if (!this.highlights$) {
       this.highlights$ = this.fetchHighlights();
     }
     return  this.highlights$;
   }
-
   private fetchHighlights(): Observable<HighlightListingsPreviews> {
     return fromFetch<HighlightListingsPreviews>(this.apiEndpoint, {
       selector: response => response.json()
@@ -39,16 +24,5 @@ export class ApiService {
       shareReplay(),
       map(response => response),
     );
-  }
-
-
-  private addPreloadFetchLink(): void {
-    this.linkService.addLink({
-      rel: 'preload',
-      as: 'fetch',
-      type: 'application/json',
-      href: this.apiEndpoint,
-      crossorigin: ''
-    });
   }
 }
