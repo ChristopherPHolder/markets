1789bd1326d8bb3dfb28ff6e9e773d7b9403d951
Chris <chris@deep-blue.io>
2023-02-02 21:44:38 +0100

    refactor: move add prefetch link to feature component

--- libs/features/src/lib/highlights/highlights.resolver.service.ts
+++ libs/features/src/lib/highlights/highlights.resolver.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
 import {  Resolve } from '@angular/router';
 import { HighlightListingsPreviews, ApiService } from 'data-access';
 import { Observable } from 'rxjs';
+import { LinkService } from "external-resource-link";
+import { isPlatformBrowser } from "@angular/common";
+import { environment } from 'environments';
 
 @Injectable({providedIn: 'root'})
-export class HighlightsResolver implements Resolve<HighlightListingsPreviews | null> {
-  constructor(private apiService: ApiService) {}
-  resolve(): Observable<HighlightListingsPreviews | null> {
-    return this.apiService.getHighlightListings();
+export class HighlightsResolver implements Resolve<HighlightListingsPreviews | void> {
+  private readonly apiEndpoint = environment.apiEndpoint;
+  private readonly isBrowser: boolean;
+  constructor(
+    @Inject(PLATFORM_ID) platformId: object,
+    private apiService: ApiService,
+    private linkService: LinkService
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+  resolve(): Observable<HighlightListingsPreviews> | void {
+    if (this.isBrowser) {
+      return this.apiService.getHighlightListings();
+    }
+    return this.linkService.addLink({
+      rel: 'preload',
+      as: 'fetch',
+      type: 'application/json',
+      href: this.apiEndpoint,
+      crossorigin: ''
+    });
   }
 }
 
