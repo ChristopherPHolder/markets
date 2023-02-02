2c639dd96ee65857c052804ce4b9d58e433ead94
Chris <chris@deep-blue.io>
2023-02-02 21:46:36 +0100

    refactor: fix browser code execution in node

--- apps/maritime/src/app/app.module.ts
+++ apps/maritime/src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ApiService } from 'data-access';
+import { APP_INITIALIZER, inject, NgModule, PLATFORM_ID } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { ApiService, HighlightListingsPreviews } from "data-access";
 import { LinkService } from "external-resource-link";
 import { HeaderComponent } from "ui";
 import { tap } from "rxjs";
+import { isPlatformBrowser } from "@angular/common";
 
 function initializeDataFactory(apiService: ApiService, linkService: LinkService): () => Promise<void> {
+  const isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
   return () => new Promise((resolve) => {
-    apiService.getHighlightListings().pipe(tap((listings) => {
-      if (!listings) return;
-      performance.mark('Prefetching LCP Images');
-      for (let i = 0; i < 2; i++) {
-        linkService.addLink({
-          rel: 'preload',
-          as: 'image',
-          href: listings["watercrafts"][i].thumbnailUrl,
-          fetchpriority: 'high'
-        });
-      }
-    })).subscribe();
+    if (isBrowser) {
+      apiService.getHighlightListings().pipe(tap((listings: HighlightListingsPreviews) => {
+        performance.mark('Prefetching LCP Images');
+        for (let i = 0; i < 2; i++) {
+          linkService.addLink({
+            rel: 'preload',
+            as: 'image',
+            href: listings["watercrafts"][i].thumbnailUrl,
+            fetchpriority: 'high'
+          });
+        }
+      })).subscribe();
+    }
     resolve();
   });
 }
