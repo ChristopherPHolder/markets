4648fdad28a23e28702898eb06319e9856f6d0f8
Chris <chris@deep-blue.io>
2023-02-02 17:15:35 +0100

    fix: fix app shell unit test

--- apps/maritime/src/app/app.component.spec.ts
+++ apps/maritime/src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 import { AppComponent } from './app.component';
+import { FooterComponent, HeaderComponent } from "ui";
+import { RouterTestingModule } from "@angular/router/testing";
+import { featuresRoutes } from "features";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [
+        HeaderComponent,
+        RouterTestingModule.withRoutes(featuresRoutes),
+      ],
     }).compileComponents();
   });
 
@@ -14,9 +21,21 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  it('should create the header', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
   it(`should have as title 'maritime'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('maritime');
+  });
+
+  it('should create the footer', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
